2413cddc7b59460a695d8dd0e62f372e620b3d1d
caifeng01 <caifeng01@baidu.com>
2023-06-29 22:34:40 +0800

    fix: prettier分号问题

--- packages/mini-koa/src/index.ts
+++ packages/mini-koa/src/index.ts
@@ -1,65 +1,65 @@
-import http from 'http'
+import http from 'http';
 
-type Next = () => Promise<void>
+type Next = () => Promise<void>;
 
-type MiddleWare = (ctx: Context, next: Next) => Promise<void>
+type MiddleWare = (ctx: Context, next: Next) => Promise<void>;
 
 class Context {
-  request: http.IncomingMessage
+  request: http.IncomingMessage;
 
   response: http.ServerResponse<http.IncomingMessage> & {
-    req: http.IncomingMessage
-  }
+    req: http.IncomingMessage;
+  };
 
   constructor(
     req: http.IncomingMessage,
     res: http.ServerResponse<http.IncomingMessage> & {
-      req: http.IncomingMessage
+      req: http.IncomingMessage;
     }
   ) {
-    this.request = req
-    this.response = res
-    return this
+    this.request = req;
+    this.response = res;
+    return this;
   }
 
   set body(value: any) {
-    this.response.end(value)
+    this.response.end(value);
   }
 }
 
 class Koa {
-  middlewares = []
+  middlewares = [];
 
   constructor() {
-    return this
+    return this;
   }
 
   use = middleware => {
-    this.middlewares.push(middleware)
-  }
+    this.middlewares.push(middleware);
+  };
 
   listen = (port?: number, hostname?: string) => {
-    const middleFunc = this.#compose(this.middlewares)
+    const middleFunc = this.#compose(this.middlewares);
     const server = http.createServer((req, res) => {
-      const ctx = new Context(req, res)
-      middleFunc(ctx)
-    })
-    server.listen(port, hostname)
-  }
+      const ctx = new Context(req, res);
+      middleFunc(ctx);
+    });
+    server.listen(port, hostname);
+  };
 
   #compose = (middlewares: MiddleWare[]) => {
     return ctx => {
       const next = middlewares.reduceRight(
         (nextFunc: Next, middleware) => {
           return async () => {
-            await middleware(ctx, nextFunc)
-          }
+            await middleware(ctx, nextFunc);
+          };
         },
         async () => {}
-      )
-      return next()
-    }
-  }
+      );
+      return next();
+    };
+  };
 }
 
-export default Koa
+export default Koa;
